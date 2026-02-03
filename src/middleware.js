599cf39455b115379c18662ab961fabe3985158a
ayushks27 <purnenduraghavsrivastava@gmail.com>
2026-02-03 23:46:12 +0530

    Simplify middleware matcher for Edge runtime

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)", 
+  "/dashboard(.*)",
 ]);
 
-// Middleware logic
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
 });
 
-// Middleware config
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/((?!_next|.*\\..*).*)",
   ],
 };
